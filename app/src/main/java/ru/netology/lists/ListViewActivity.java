69f5b8f58e408c916f65b66981fd996d3dec7e89
Puten <afaputen@list.ru>
2019-04-28 16:16:23 +0300

    task2

--- app/src/main/java/ru/netology/lists/ListViewActivity.java
+++ app/src/main/java/ru/netology/lists/ListViewActivity.java
@@ -1,14 +1,18 @@
 package ru.netology.lists;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,6 +20,10 @@ import java.util.List;
 import java.util.Map;
 
 public class ListViewActivity extends AppCompatActivity {
+    List<Map<String, String>> simpleAdapterContent = new ArrayList<>();
+    private SharedPreferences myListSharedPref;
+    BaseAdapter listContentAdapter;
+    SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,37 +32,72 @@ public class ListViewActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ListView list = findViewById(R.id.list);
+        swipeRefreshLayout = findViewById(R.id.swipeRefresh);
 
-        List<Map<String, String>> values = prepareContent();
+        ListView listView = findViewById(R.id.list);
 
-        BaseAdapter listContentAdapter = createAdapter(values);
+        myListSharedPref = getSharedPreferences("MyList", MODE_PRIVATE);
 
-        list.setAdapter(listContentAdapter);
+        if (!myListSharedPref.contains("MyList")) {
+            SharedPreferences.Editor myEditor = myListSharedPref.edit();
+            myEditor.putString("MyList", getString(R.string.large_text));
+            myEditor.apply();
+            Toast.makeText(ListViewActivity.this, "данные сохранены", Toast.LENGTH_LONG).show();
+        }
+
+        prepareContent(myListSharedPref.getString("MyList", ""));
+        listContentAdapter = createAdapter();
+
+        listView.setAdapter(listContentAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                simpleAdapterContent.remove(position);
+                listContentAdapter.notifyDataSetChanged();
+
+                Toast.makeText(
+                        ListViewActivity.this,
+                        "Position " + position + " removed",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Toast.makeText(
+                        ListViewActivity.this,
+                        "Swipe Refresh",
+                        Toast.LENGTH_LONG).show();
+
+                prepareContent(myListSharedPref.getString("MyList", ""));
+                listContentAdapter.notifyDataSetChanged();
+                
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
     }
 
     @NonNull
-    private BaseAdapter createAdapter(List<Map<String, String>> values) {
+    private BaseAdapter createAdapter() {
         return new SimpleAdapter(
                 this,
-                values,
+                simpleAdapterContent,
                 R.layout.my_list_item,
                 new String[]{"text", "len"}, new int[]{R.id.textView1, R.id.textView2});
     }
 
-    @NonNull
-    private List<Map<String, String>> prepareContent() {
-        String[] strList = getString(R.string.large_text).split("\n\n");
-        List<Map<String, String>> content = new ArrayList<>();
+    void prepareContent(String listText) {
+        simpleAdapterContent.clear();
+
+        String[] strList = listText.split("\n\n");
 
         for (String str : strList) {
             Map<String, String> row = new HashMap<>();
             row.put("text", str);
             row.put("len", String.valueOf(str.length()));
 
-            content.add(row);
+            simpleAdapterContent.add(row);
         }
-
-        return content;
     }
 }
